d6805b7dbc40ca4f7a9899b1819df0ddc5534ff9
VadimVM <vadimvm@gmail.com>
2021-03-05 15:48:57 +0200

    new search

---
+++ src/main/java/com/vadym/pages/driverManager/LocalWebDriver.java
@@ -0,0 +1,29 @@
+package com.vadym.pages.driverManager;
+
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideConfig;
+import com.codeborne.selenide.WebDriverProvider;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import javax.annotation.Nonnull;
+
+public class LocalWebDriver implements WebDriverProvider {
+
+    @Nonnull
+    @Override
+    public WebDriver createDriver(@Nonnull DesiredCapabilities desiredCapabilities) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("–no-sandbox");
+        return new ChromeDriver(options);
+    }
+}
